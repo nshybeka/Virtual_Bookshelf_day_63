a1302a4b1a615c1b299cc37eba363afa14946537
Natallia Shybeka <n.shybeka@gmail.com>
2023-07-11 11:01:10 +0400

    add new book

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-all_books = []
+all_books = [
+    {"title": "Book 1", "author": "Author 1", "rating": 8},
+    {"title": "Book 2", "author": "Author 2", "rating": 7},
+    {"title": "Book 3", "author": "Author 3", "rating": 9}
+]
 
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
 
