17927b6ee2adbeb48b3231ffa128a6346a2aba9c
Natallia Shybeka <n.shybeka@gmail.com>
2023-07-13 10:31:15 +0400

    Completed project

--- main.py
+++ main.py
@@ -59,6 +59,19 @@ def edit():
     return render_template("edit_rating.html", book=book_selected)
 
 
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
 
