31a81bc2905b55494ebcbb6cc66bc48708a436f7
Natallia Shybeka <n.shybeka@gmail.com>
2023-07-13 09:57:31 +0400

    Add SQLAlchemy

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = [
-    {"title": "Book 1", "author": "Author 1", "rating": 8},
-    {"title": "Book 2", "author": "Author 2", "rating": 7},
-    {"title": "Book 3", "author": "Author 3", "rating": 9}
-]
+# CREATE DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my-books-collection.db"
+# Create the extension
+db = SQLAlchemy()
+# initialise the app with the extension
+db.init_app(app)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html', books=all_books)
+# CREATE RECORD
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), unique=True, nullable=False)
+    rating = db.Column(db.FLOAT, nullable=False)
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
 
